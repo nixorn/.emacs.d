0ae546b42bdbca589ce1ef184ed9ad2e34e5f514
Artem Kanev <a-kanev@yandex.ru>
2019-09-11 23:46:07 +0300

    Install some haskell stuff

---
+++ elpa/lsp-haskell-20190602.825/lsp-haskell-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "lsp-haskell" "20190602.825" "Haskell support for lsp-mode" '((lsp-mode "3.0") (haskell-mode "1.0")) :commit "64106be79350f9ce6903d22c66b29761dadb5001" :keywords '("haskell") :url "https://github.com/emacs-lsp/lsp-haskell")
