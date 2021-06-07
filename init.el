1279eff593237bb416b4fd710e8674c3f1e82689
Artem Kanev <a-kanev@yandex.ru>
2021-06-07 14:01:42 +0300

    Migration

--- init.el
+++ init.el
@@ -4,6 +4,166 @@
 
 (require 'macros)
 
+(setq create-lockfiles nil)
+
+(setq make-backup-files nil)
+
+(setq package-archives '(("melpa" . "http://melpa.org/packages/")
+                         ("gnu" . "http://elpa.gnu.org/packages/")))
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+;;;;;;;;;;; RUST
+;;;; see https://robert.kra.hn/posts/2021-02-07_rust-with-emacs/
+
+(use-package rustic
+  :ensure
+  :bind (:map rustic-mode-map
+              ("M-j" . lsp-ui-imenu)
+              ("M-?" . lsp-find-references)
+              ("C-c C-c l" . flycheck-list-errors)
+              ("C-c C-c a" . lsp-execute-code-action)
+              ("C-c C-c r" . lsp-rename)
+              ("C-c C-c q" . lsp-workspace-restart)
+              ("C-c C-c Q" . lsp-workspace-shutdown)
+              ("C-c C-c s" . lsp-rust-analyzer-status)
+              ("C-c C-c e" . lsp-rust-analyzer-expand-macro)
+              ("C-c C-c d" . dap-hydra)
+              ("C-c C-c h" . lsp-ui-doc-glance))
+  :config
+  ;; uncomment for less flashiness
+  ;; (setq lsp-eldoc-hook nil)
+  ;; (setq lsp-enable-symbol-highlighting nil)
+  ;; (setq lsp-signature-auto-activate nil)
+
+  ;; comment to disable rustfmt on save
+  (setq rustic-format-on-save nil)
+  (add-hook 'rustic-mode-hook 'rk/rustic-mode-hook)
+  (add-hook 'rustic-mode-hook  #'enable-paredit-mode)
+
+  )
+
+(defun rk/rustic-mode-hook ()
+  ;; so that run C-c C-c C-r works without having to confirm
+  (setq-local buffer-save-without-query t))
+
+;; -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+;; for rust-analyzer integration
+
+(use-package lsp-mode
+   :ensure
+   :commands lsp
+   :custom
+   ;; what to use when checking on-save. "check" is default, I prefer clippy
+   (lsp-rust-analyzer-cargo-watch-command "clippy")
+   (lsp-eldoc-render-all t)
+   (lsp-idle-delay 0.6)
+   (lsp-rust-analyzer-server-display-inlay-hints t)
+   :config
+   (add-hook 'lsp-mode-hook 'lsp-ui-mode))
+
+(use-package lsp-ui
+  :ensure
+  :commands lsp-ui-mode
+  :custom
+  (lsp-ui-peek-always-show t)
+  (lsp-ui-sideline-show-hover t)
+  (lsp-ui-doc-enable nil))
+
+
+;; -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+;; inline errors
+
+(use-package flycheck :ensure)
+
+
+;; -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+;; auto-completion and code snippets
+
+(use-package yasnippet
+  :ensure
+  :config
+  (yas-reload-all)
+  (add-hook 'prog-mode-hook 'yas-minor-mode)
+  (add-hook 'text-mode-hook 'yas-minor-mode))
+
+(use-package company
+  :ensure
+  :bind
+  (:map company-active-map
+              ("C-n". company-select-next)
+              ("C-p". company-select-previous)
+              ("M-<". company-select-first)
+              ("M->". company-select-last))
+  (:map company-mode-map
+        ("<tab>". tab-indent-or-complete)
+        ("TAB". tab-indent-or-complete)))
+
+(defun company-yasnippet-or-completion ()
+  (interactive)
+  (or (do-yas-expand)
+      (company-complete-common)))
+
+(defun check-expansion ()
+  (save-excursion
+    (if (looking-at "\\_>") t
+      (backward-char 1)
+      (if (looking-at "\\.") t
+        (backward-char 1)
+        (if (looking-at "::") t nil)))))
+
+(defun do-yas-expand ()
+  (let ((yas/fallback-behavior 'return-nil))
+    (yas/expand)))
+
+(defun tab-indent-or-complete ()
+  (interactive)
+  (if (minibufferp)
+      (minibuffer-complete)
+    (if (or (not yas/minor-mode)
+            (null (do-yas-expand)))
+        (if (check-expansion)
+            (company-complete-common)
+          (indent-for-tab-command)))))
+
+
+;; -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+;; for Cargo.toml and other config files
+
+(use-package toml-mode :ensure)
+
+
+;; -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+;; setting up debugging support with dap-mode
+
+(use-package exec-path-from-shell
+  :ensure
+  :init (exec-path-from-shell-initialize))
+
+(when (executable-find "lldb-mi")
+  (use-package dap-mode
+    :ensure
+    :config
+    (dap-ui-mode)
+    (dap-ui-controls-mode 1)
+
+    (require 'dap-lldb)
+    (require 'dap-gdb-lldb)
+    ;; installs .extension/vscode
+    (dap-gdb-lldb-setup)
+    (dap-register-debug-template
+     "Rust::LLDB Run Configuration"
+     (list :type "lldb"
+           :request "launch"
+           :name "LLDB::Run"
+	   :gdbpath "rust-lldb"
+           ;; uncomment if lldb-mi is not in PATH
+           ;; :lldbmipath "path/to/lldb-mi"
+           ))))
+;;;;;;;;;;;;;;;;;;;;
+
 (global-set-key "\C-z" 'undo)
 (global-set-key (kbd "M-g") 'rgrep)
 (global-set-key (kbd "\C-x i") 'previous-multiframe-window)
@@ -34,18 +194,15 @@
 (eval-after-load "haskell-mode"
   '(define-key haskell-mode-map (kbd "C-c C-c") 'haskell-compile))
 
-(add-hook 'haskell-mode-hook #'haskell-collapse-mode)
-(add-hook 'rust-mode-hook #'cargo-minor-mode)
-
-(global-set-key "\C-t" 'haskell-hide-toggle)
-(setq haskell-compile-cabal-build-command "stack install --test")
+(setq haskell-compile-cabal-build-command "stack install --pedantic")
+;; (setq haskell-compile-cabal-build-command "stack install --ghc-options=\"-O0\" --pedantic")
 ;; (setq haskell-compile-cabal-build-command "nix-shell --command 'cabal build' --attr env release.nix")
 ;; (setq haskell-compile-cabal-build-command "NIXPKGS_ALLOW_BROKEN=1 nix-build")
 (setq-default show-trailing-whitespace t)
 (setq-default haskell-tags-on-save t)
 (setq-default tags-revert-without-query t)
 
-;;;;;;;;;;;
+
 
 (let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
                     (not (gnutls-available-p))))
@@ -70,27 +227,26 @@ There are two things you can do about this warning:
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (wombat)))
+ '(custom-enabled-themes '(wombat))
  '(dhall-format-at-save nil)
  '(dhall-type-check-inactivity-timeout 5000)
- '(haskell-process-type (quote stack-ghci))
+ '(haskell-process-type 'stack-ghci)
  '(haskell-stylish-on-save t)
  '(indent-tabs-mode nil)
+ '(js-indent-level 2)
+ '(lsp-haskell-formatting-provider "stylish-haskell")
  '(package-selected-packages
-   (quote
-    (cargo php-mode typescript-mode paredit web-beautify multiple-cursors dhall-mode git-gutter magit lsp-haskell)))
+   '(csv-mode tide company flycheck-haskell lsp-ui lsp-mode yaml-mode php-mode typescript-mode paredit web-beautify multiple-cursors dhall-mode git-gutter magit lsp-haskell))
  '(require-final-newline t)
  '(sql-connection-alist
-   (quote
-    (("local"
-      (sql-product
-       (quote postgres))
+   '(("local"
+      (sql-product 'postgres)
       (sql-user "nixorn")
       (sql-database "getshoptv_source")
-      (sql-server "localhost")))))
+      (sql-server "localhost"))))
  '(tab-width 2)
  '(typescript-indent-level 2)
- '(version-control (quote never)))
+ '(version-control 'never))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -136,7 +292,6 @@ There are two things you can do about this warning:
 (add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
 (add-hook 'dhall-mode-hook       #'enable-paredit-mode)
 (add-hook 'haskell-mode-hook       #'enable-paredit-mode)
-(add-hook 'rust-mode-hook       #'enable-paredit-mode)
 (add-hook 'python-mode-hook       #'enable-paredit-mode)
 (global-set-key "{" 'paredit-open-curly)
 
@@ -160,3 +315,17 @@ There are two things you can do about this warning:
 ;; split windows side-by-side
 (setq split-height-threshold nil)
 (setq split-width-threshold 70)
+
+
+;; kdb macros
+
+(fset 'format-stack-errors
+   [?\M-x ?r ?e ?a ?d ?- ?o ?n ?l ?y ?- ?m ?o ?d ?e return ?\M-x ?r ?e ?p ?l ?a ?c ?s backspace ?e ?- ?r ?e tab ?g tab return ?\[ ?a ?- ?z ?\] ?* ?> ?  return return escape ?< tab return])
+
+
+
+
+(defun insert-file-name ()
+  "Insert the full path file name into the current buffer."
+  (interactive)
+  (insert (buffer-file-name (window-buffer (minibuffer-selected-window)))))
